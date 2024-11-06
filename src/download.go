1db8e023be67590a3a1eba44d4d318710b93586f
amaumene <alexandre@maumene.org>
2024-11-06 13:46:13 +0900

    Add support for downloading via Got
    Refactored download function to use the Got package for handling downloads, including progress tracking and improved concurrency. This enhancement allows for more efficient downloads with detailed progress reporting. Added context handling for robust cancellation and a new user agent for better compatibility.

--- src/download.go
+++ src/download.go
@@ -1,7 +1,54 @@
 package main
 
-import "github.com/amaumene/momenarr/torbox"
+import (
+	"context"
+	"fmt"
+	"github.com/amaumene/momenarr/got"
+	"github.com/amaumene/momenarr/torbox"
+	"path/filepath"
+)
 
-func downloadFromTorBox(UsenetDownload torbox.UsenetDownload, appConfig App) error {
+func downloadWithGot(fileLink string, UsenetDownload []torbox.UsenetDownload, appConfig App) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	download := got.NewDownload(ctx, fileLink, filepath.Join(appConfig.downloadDir, UsenetDownload[0].Files[0].ShortName))
+	download.Concurrency = 4
+	download.ChunkSize = 1073741824 // 1GiB
+	download.Interval = 10000
 
+	got.UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15"
+
+	if err := download.Init(); err != nil {
+		fmt.Println(err)
+	}
+	go func() {
+		if err := download.Start(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	download.RunProgress(func(d *got.Download) {
+		fmt.Printf("Downloading %s: %d MB/s / %d GB\n",
+			UsenetDownload[0].Files[0].ShortName,
+			download.AvgSpeed()/1024/1024,
+			download.Size()/1024/1024,
+		)
+	})
+
+	cancel()
+	fmt.Println("Download started")
+	return nil
+}
+
+func showProgress() func(ctx context.Context, download *got.Download, cancel context.CancelFunc) {
+	return nil
+}
+
+func downloadFromTorBox(UsenetDownload []torbox.UsenetDownload, appConfig App) error {
+	fileLink, err := appConfig.TorBoxClient.RequestUsenetDownloadLink(UsenetDownload)
+	if err != nil {
+		return err
+	}
+	downloadWithGot(fileLink, UsenetDownload, appConfig)
+	return nil
 }
