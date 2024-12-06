8a22ccd8c68246cd66165c36fbda7b3d6e3acda2
amaumene <alexandre@maumene.org>
2024-12-06 12:28:53 +0900

    Simplify error messages and improve IMDB ID validation.
    The error messages in the HTTP request handling were made more concise by removing redundant "error" terms. Additionally, an IMDB ID validation step was added to ensure its length exceeds two characters before accessing its substring, preventing potential errors.

--- src/newsnab/newsnab.go
+++ src/newsnab/newsnab.go
@@ -12,44 +12,48 @@ func SearchTVShow(TVDB int64, showSeason int64, showEpisode int64, newsNabHost s
 	// Make the HTTP GET request
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", fmt.Errorf("error making request: %v", err)
+		return "", fmt.Errorf("making request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	// Check if the request was successful
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("error: did not receive a 200 OK status, received %d", resp.StatusCode)
+		return "", fmt.Errorf("did not receive a 200 OK status, received %d", resp.StatusCode)
 	}
 
 	// Read the body of the response
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("error reading response body: %v", err)
+		return "", fmt.Errorf("reading response body: %v", err)
 	}
 
 	return string(body), nil
 }
 
 func SearchMovie(IMDB string, newsNabHost string, newsNabApiKey string) (string, error) {
-	nzbIMDB := IMDB[2:]
+	if len(IMDB) > 2 {
+		IMDB = IMDB[2:]
+	} else {
+		return "", fmt.Errorf("invalid IMDB ID")
+	}
 	// Construct the URL with the provided arguments
-	url := fmt.Sprintf("https://%s/api?apikey=%s&t=movie&imdbid=%s&o=json", newsNabHost, newsNabApiKey, nzbIMDB)
+	url := fmt.Sprintf("https://%s/api?apikey=%s&t=movie&imdbid=%s&o=json", newsNabHost, newsNabApiKey, IMDB)
 	// Make the HTTP GET request
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", fmt.Errorf("error making request: %v", err)
+		return "", fmt.Errorf("making request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	// Check if the request was successful
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("error: did not receive a 200 OK status, received %d", resp.StatusCode)
+		return "", fmt.Errorf("did not receive a 200 OK status, received %d", resp.StatusCode)
 	}
 
 	// Read the body of the response
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("error reading response body: %v", err)
+		return "", fmt.Errorf("reading response body: %v", err)
 	}
 
 	return string(body), nil
